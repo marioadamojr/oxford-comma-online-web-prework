ce6bfce077af24bb4f1a0d2c6281b90fe906f92d
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 21:58:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
   if array.length == 1
-    puts array.join
+    return array.join
   elsif array.length == 2
-    puts array.join(", ")
+    return array.join(", ")
   else
     last = array.pop
     last = last.join
     incl_and = array.insert(-1, "and ")
     words = incl_and.join(", ")
-    puts words + last
+    return words + last
   end
 end
