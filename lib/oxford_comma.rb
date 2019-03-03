eae7842312f289c7f58a472369d10d923ca5b843
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 21:59:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,6 @@ def oxford_comma(array)
     return array.join(" and ")
   else
     last = array.pop
-    last = last.join
     incl_and = array.insert(-1, "and ")
     words = incl_and.join(", ")
     return words + last
