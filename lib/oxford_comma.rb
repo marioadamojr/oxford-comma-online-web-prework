a48ecd0f743702d3bff50cc2597cfd72761c4012
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 22:00:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last = array.pop
+    incl_and = array.insert(-1, "and ")
+    words = incl_and.join(", ")
+    return words + last
+  end
 end
