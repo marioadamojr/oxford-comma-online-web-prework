efab1b08778f8194f8de260a7c14bb2d6ed18963
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 21:59:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
   elsif array.length == 2
-    return array.join(", ")
+    return array.join(" and ")
   else
     last = array.pop
     last = last.join
